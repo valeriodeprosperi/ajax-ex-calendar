40faeda7024ba95e5443aed8d1c16ec650fbfbb5
valerio <valerio.deprosperi@gmail.com>
2020-09-21 16:07:31 +0200

    template calendario

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+$(document).ready(function){
 
+// data iniziale Calendario
+var date = "2018-02-01";
+
+var momentDate = moment(date);
+
+// template giorni del mio Calendario
+var source = $("#day-template").html();
+var template = Handlebars.compile(source);
+
+// stampo i giorni del mio Calendario
+for (var i = 1; i <= momentDate.daysInMonth(); i++) {
+
+
+  
+}
+
+
+
+}
