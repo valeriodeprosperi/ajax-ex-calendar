62d4517b06857baab8fac10c4315478bf3a49dd0
valerio <valerio.deprosperi@gmail.com>
2020-09-21 16:14:56 +0200

    modifiche js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function){
+$(document).ready(function(){
 
 // data iniziale Calendario
 var date = "2018-02-01";
@@ -12,10 +12,24 @@ var template = Handlebars.compile(source);
 // stampo i giorni del mio Calendario
 for (var i = 1; i <= momentDate.daysInMonth(); i++) {
 
+var dateCompleteMoment = momentDate;
 
-  
+var context = {
+  "day": i,
+  "month": momentDate.format("MMMM"),
+  "dateComplete": momentDate.format(YYYY-MM-DD)
+};
+
+var html= template(context);
+
+$("days").append(html);
+
+dateCompleteMoment.add(1, "day");
+
 }
 
 
 
-}
+
+
+});
